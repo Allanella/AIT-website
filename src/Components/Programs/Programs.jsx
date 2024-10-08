db0baad73322914b8a4ac738917ac511d7652d6f
Allanella <allanbaliddawa@gmail.com>
2024-10-08 16:57:36 -0700

    first commit

---
+++ src/Components/Programs/Programs.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Programs.css'
+import coda from '../../assets/coda.jpg'
+import codi from '../../assets/codi.jpg'
+import myHand from '../../assets/myHand.jpg'
+
+const Programs = () => {
+  return (
+    <div className='progs'>
+
+      <div className='progra'>
+s
+        <img src={coda} alt='' />
+      </div>
+
+
+      <div className='progra'>
+        <img src={codi} alt='' />
+
+
+
+
+      </div>
+
+
+      <div className='progra'>
+
+        <img src={myHand} alt='' />
+      </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default Programs
---
+++ src/Components/Tech/Tech.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Tech.css'
+
+const Tech = () => {
+  return (
+    <div className='tech navContainer'>
+      <div className='MyTechText'>
+        <h1>Crafting Unique Online Experiences</h1>
+        <p>Our commitment is to assist our clients to achieve a more competitive and profitable situation,
+          applying the principles of organization,
+          engineering, methodology and human resources.
+          We develop and apply technologically innovating solutions
+          to satisfy the customer's needs and goals.</p>
+
+        <button className='myButton'>Explore more</button>
+
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Tech;
