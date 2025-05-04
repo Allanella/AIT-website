f898b6bee69f3ad3204caae611e5afb5837d318e
Allanella <allanbaliddawa@gmail.com>
2025-05-04 12:31:58 +0300

    Added some final frontend code

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -6,6 +6,7 @@ import lillian from "../../assets/lillian.jpg";
 import sam from "../../assets/sam.jpg";
 import allanella from "../../assets/allanella.jpg";
 import torren from "../../assets/torren.jpg";
+import JORDI from "../../assets/JORDI.jpg"
 import back from "../../assets/back.jpg";
 import next from "../../assets/next.jpg";
 
@@ -41,7 +42,6 @@ const Testimonials = () => {
               </div>
               <div>
                 <h3>Nyombi Elijah</h3>
-                
               </div>
               <p>
                 Elijah Nyombi is one of the Directors for iONA Tech, a
@@ -64,20 +64,29 @@ const Testimonials = () => {
                 <img className="Mylillian" src={lillian} alt="" />
               </div>
               <div>
-                <h3>Lillian</h3>
-                
+                <h3>Nakunda Lillian</h3>
               </div>
               <p>
-                I am hard working and enthusiastic about learning new skills. I
-                am well organised and always meet deadlines when they have been
-                set. I find it easy working with other people but can also work
-                well independently. I have shown that I am a trustworthy and
-                responsible person in school and at home. I have got very good
-                communication skills which I feel will be useful when working in
-                new situations. I enjoy travelling and have travelled to many
-                different places within the UK and abroad. These experiences
-                will be useful within the travel industry. I am keen to further
-                develop my experiences and skills within the travel business.{" "}
+                Nankunda Lilian, a young but innovative Software Engineering
+                student at Makerere University - Uganda , i also take on other
+                self-study like short small online courses and practical
+                skilling . As a young lady developer, I am currently
+                Practicing Systems Analyst, Web Developer, Mobile Developer and
+                Graphic Designer in the tech space. I specialize in front-end
+                web development and front-end mobile development: transforming
+                user interfaces into clean, interactive, and responsive
+                experiences. But when the need arises, I also get into backend
+                development thereby becoming a full stack developer. I Play With
+                A Lot Of Different Languages And Things Like…Kotlin & Java – For
+                Android And Backend Logic JavaScript (And React Native) – For
+                Cross-Platform Mobile AppsPHP, HTML & CSS – For Dynamic And
+                Beautiful Websites. Graphic Design – Bridging creativity
+                with tech while making User experience easier through graphics
+                I am a I scalable and flexible young lady ready to learn as long
+                as my world demands it for the success of development in a
+                continuously changing, and evolving computing universe, i am
+                ready to with it by adopting new tools, frameworks and practices
+                to improve more solutions for quality work.{" "}
               </p>
             </div>
           </li>
@@ -89,7 +98,6 @@ const Testimonials = () => {
               </div>
               <div>
                 <h3>Baliddawa Allan</h3>
-                
               </div>
               <p>
                 I'm a passionate and skilled Fullstack Web Developer with a
@@ -107,26 +115,59 @@ const Testimonials = () => {
             </div>
           </li>
 
+
+          <li>
+            <div className="slide">
+              <div className="UserInfo">
+                <img className="jordi" src={JORDI} alt="" />
+              </div>
+              <div>
+                <h3>Mulungi Abigail</h3>
+              </div>
+              <p>
+                Abigail is a passionate JavaScript developer with a keen eye for innovation.
+                 With a strong focus on crafting exceptional software experiences,
+                  She excels in designing and developing scalable web applications and desktop solutions.
+                  With a dedication to staying up-to-date on the latest industry trends and technologies.
+                  Abigail continually enhances her skills to delive high quality solutions that exceed expectations. {" "}
+              </p>
+            </div>
+          </li>
+
           <li>
             <div className="slide">
               <div className="UserInfo">
                 <img className="MyAlien" src={alien} alt="" />
               </div>
               <div>
-                <h3>Alien</h3>
-                
+                <h3>Mpairwe Lauben</h3>
               </div>
               <p>
-                I am hard working and enthusiastic about learning new skills. I
-                am well organised and always meet deadlines when they have been
-                set. I find it easy working with other people but can also work
-                well independently. I have shown that I am a trustworthy and
-                responsible person in school and at home. I have got very good
-                communication skills which I feel will be useful when working in
-                new situations. I enjoy travelling and have travelled to many
-                different places within the UK and abroad. These experiences
-                will be useful within the travel industry. I am keen to further
-                develop my experiences and skills within the travel business.{" "}
+                With over five years of hands-on experience, Mpairwe Lauben is a
+                dynamic software engineer with a passion for crafting
+                intelligent, scalable, and user-centric digital solutions. His
+                core expertise lies at the intersection of mobile application
+                development, cloud-native systems, and machine learning
+                technologies, where he seamlessly bridges intuitive user
+                experiences with powerful backend intelligence. Lauben has led
+                and contributed to high-impact projects across various domains,
+                architecting mobilefirst solutions that leverage the full
+                potential of cloud infrastructure and AI-driven insights. His
+                proficiency spans across modern tech stacks, including Flutter,
+                React Native, AWS, Firebase, TensorFlow, and Kubernetes,
+                enabling him to engineer products that are both technically
+                robust and elegantly designed. An advocate for clean
+                architecture and agile development, Lauben brings a
+                problem-solving mindset, a commitment to innovation, and a deep
+                understanding of scalable system design. Whether he’s building
+                mission-critical mobile apps, deploying cloud solutions, or
+                training intelligent models, he brings precision, performance,
+                and a future-forward perspective to every line of code. Outside
+                of code, Lauben actively engages in developer communities,
+                mentoring emerging engineers and staying ahead of industry
+                trends. His work reflects a rare blend of technical mastery,
+                creativity, and a deep commitment to delivering technology that
+                makes a meaningful impact.{" "}
               </p>
             </div>
           </li>
@@ -138,7 +179,6 @@ const Testimonials = () => {
               </div>
               <div>
                 <h3>Katongole Samuel</h3>
-                
               </div>
               <p>
                 samuel is a passionate java Developer dedicated to crafting
@@ -159,8 +199,7 @@ const Testimonials = () => {
                 <img className="Mytorren" src={torren} alt="" />
               </div>
               <div>
-                <h3>Torren</h3>
-               
+                <h3> Amumpire Torren</h3>
               </div>
               <p>
                 As a digital enthusiast, AI activist and skilled graphics
